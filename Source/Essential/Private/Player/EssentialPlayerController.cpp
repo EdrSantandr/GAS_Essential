6fec03c3eca5deb97cd78635d69283e6209c58c2
Eder Santander <eder@bamtang.com>
2024-01-13 11:38:58 -0500

    Created Playercontroller and IMC to handle movement, but still waiting for mouse actions

---
+++ Source/Essential/Private/Player/EssentialPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright EdrSantandr
+
+
+#include "Player/EssentialPlayerController.h"
+
---
+++ Source/Essential/Public/Player/EssentialPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright EdrSantandr
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EssentialPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESSENTIAL_API AEssentialPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
