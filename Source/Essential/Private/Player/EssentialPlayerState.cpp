80cae3a0ee786119c8b4c857863301db6922553d
Eder Santander <eder@bamtang.com>
2024-01-15 18:05:18 -0500

    Playerstate class created

---
+++ Source/Essential/Private/Player/EssentialPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright EdrSantandr
+
+
+#include "Player/EssentialPlayerState.h"
+
+AEssentialPlayerState::AEssentialPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Source/Essential/Public/Player/EssentialPlayerState.h
@@ -0,0 +1,19 @@
+// Copyright EdrSantandr
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "EssentialPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESSENTIAL_API AEssentialPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+public:
+	AEssentialPlayerState();
+};
