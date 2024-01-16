cebce3005e8051f5b72eec175e30bda9f501d4ef
Eder Santander <eder@bamtang.com>
2024-01-16 07:04:23 -0500

    Vital Attributes added Just Health and Essence

--- Source/Essential/Private/AbilitySystem/EssentialAttributeSet.cpp
+++ Source/Essential/Private/AbilitySystem/EssentialAttributeSet.cpp
@@ -2,4 +2,39 @@
 
 
 #include "AbilitySystem/EssentialAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UEssentialAttributeSet::UEssentialAttributeSet()
+{
+}
+
+void UEssentialAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UEssentialAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEssentialAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEssentialAttributeSet, Essence, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEssentialAttributeSet, MaxEssence, COND_None, REPNOTIFY_Always);
+}
+
+void UEssentialAttributeSet::OnRep_Essence(const FGameplayAttributeData& OldEssence) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEssentialAttributeSet, Essence, OldEssence);
+}
+
+void UEssentialAttributeSet::OnRep_MaxEssence(const FGameplayAttributeData& OldMaxEssence) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEssentialAttributeSet, MaxEssence, OldMaxEssence);
+}
+
+void UEssentialAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEssentialAttributeSet, Health, OldHealth);
+}
+
+void UEssentialAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEssentialAttributeSet, MaxHealth, OldMaxHealth);
+}
--- Source/Essential/Public/AbilitySystem/EssentialAttributeSet.h
+++ Source/Essential/Public/AbilitySystem/EssentialAttributeSet.h
@@ -13,5 +13,33 @@ UCLASS()
 class ESSENTIAL_API UEssentialAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UEssentialAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_Health", Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxHealth", Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_Essence", Category = "Vital Attributes")
+	FGameplayAttributeData Essence;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = "OnRep_MaxEssence", Category = "Vital Attributes")
+	FGameplayAttributeData MaxEssence;
+	
+	UFUNCTION()
+	void OnRep_Essence(const FGameplayAttributeData& OldEssence) const;
+
+	UFUNCTION()
+	void OnRep_MaxEssence(const FGameplayAttributeData& OldMaxEssence) const;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
 	
 };
