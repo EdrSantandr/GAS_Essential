f4884c403be18d96680d43a171dcd11e75e414ec
Eder Santander <eder@bamtang.com>
2024-01-18 09:50:01 -0500

    Update effect actor to apply changes with a gameplayeffectclass

--- Source/Essential/Private/Actor/EssentialEffectActor.cpp
+++ Source/Essential/Private/Actor/EssentialEffectActor.cpp
@@ -2,43 +2,31 @@
 
 
 #include "Actor/EssentialEffectActor.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/EssentialAttributeSet.h"
-#include "Components/SphereComponent.h"
-
-// Sets default values
 AEssentialEffectActor::AEssentialEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(MeshComponent);
-	SphereComponent = CreateDefaultSubobject<USphereComponent>("Sphere");
-	SphereComponent->SetupAttachment(GetRootComponent());
-}
 
-void AEssentialEffectActor::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: const_cast should not be used like this 
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UEssentialAttributeSet* EssentialAttributeSet = Cast<UEssentialAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UEssentialAttributeSet::StaticClass()));
-		UEssentialAttributeSet* MutableEssenceAttributeSet = const_cast<UEssentialAttributeSet*>(EssentialAttributeSet);
-		MutableEssenceAttributeSet->SetHealth(EssentialAttributeSet->GetHealth() + 25.f);
-		Destroy();
-	}
-}
-
-void AEssentialEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void AEssentialEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &AEssentialEffectActor::OnBeginOverlap);
-	SphereComponent->OnComponentEndOverlap.AddDynamic(this, &AEssentialEffectActor::OnEndOverlap);
+}
+
+void AEssentialEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target))
+	{
+		check(GameplayEffectClass);
+		FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+		EffectContextHandle.AddSourceObject(this);
+		const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+		TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());	
+	}
 }
 
 
--- Source/Essential/Public/Actor/EssentialEffectActor.h
+++ Source/Essential/Public/Actor/EssentialEffectActor.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "EssentialEffectActor.generated.h"
 
+class UGameplayEffect;
 class USphereComponent;
 
 UCLASS()
@@ -16,19 +17,12 @@ class ESSENTIAL_API AEssentialEffectActor : public AActor
 public:	
 	AEssentialEffectActor();
 	
-	UFUNCTION()
-	virtual void OnBeginOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
-
-	UFUNCTION()
-	virtual void OnEndOverlap( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-	
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> SphereComponent;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> MeshComponent;
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	
+	UPROPERTY(EditAnywhere, Category="Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+	
 };
